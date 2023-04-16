a41018c3311cb65f6ec36a416f7725fd2407957d
Sergei Perepelkin <sergperep@Sergeis-MBP.home>
2023-04-16 17:25:26 +0200

    Add polygone demo

---
+++ draw/polygone/sketch.js
@@ -0,0 +1,27 @@
+require("p5");
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background("black");
+  stroke("white");
+  noFill();
+
+  polygon(width / 2, height / 2, 120, 6, PI / 6);
+}
+
+function polygon(x, y, radius, sides = 3, angle = 0) {
+  beginShape();
+  for (let i = 0; i < sides; i++) {
+    const a = angle + TWO_PI * (i / sides);
+    let sx = x + cos(a) * radius;
+    let sy = y + sin(a) * radius;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
+}
